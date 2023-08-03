bf0ede06d71f3879d9fdefacd9432f60cf00d9dc
Thomas Wong <thomaskwwong7@gmail.com>
2023-08-03 13:42:28 -0400

    started work on budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+
+
+
+
 const Budget = () => {
+    const { dispatch } = useContext(AppContext);
     const { budget } = useContext(AppContext);
+
+    //function to increase budget by 10
+    const increaseBudget = (name) => {
+        const increase = budget + 10;
+
+    dispatch({
+        type: 'SET_BUDGET',
+        payload: increase
+    });
+
+    }
+
+    
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>
+                Budget: £{budget}
+                {/* <button onClick={increaseBudget}>+</button> */}
+            </span>
         </div>
     );
 };
